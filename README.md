7d0788eaed2de2a80fc2d704655cf73bedc913ad
ctf0 <muah003@gmail.com>
2019-12-07 21:01:57 +0200

    init

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# vscode-auto-set-syntax
+auto set syntax based on first line content, similar to https://github.com/jfcherng/Sublime-AutoSetSyntax
+
+## Usage
+
+- create a new file
+- add something to the first line ex.`<?php`
+- save file "syntax/language is applied automatically"
+
+## Config
+
+- for syntax check https://code.visualstudio.com/docs/languages/identifiers
+
+```json
+"auto_set_syntax.syntax_mapping": [
+    {
+        "first_line_pattern": "<?php",
+        "languageId": "php"
+    }
+]
+```
---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import {ExtensionContext, workspace, languages} from 'vscode'
+const escapeStringRegexp = require('escape-string-regexp')
+
+export function activate(context: ExtensionContext) {
+    context.subscriptions.push(workspace.onDidSaveTextDocument((doc: any) => applySyntax(doc)))
+}
+
+function getConfig() {
+    return workspace.getConfiguration('auto_set_syntax')
+}
+
+function applySyntax(doc: any) {
+    let fLine = doc.lineAt(0)
+
+    if (!fLine.isEmptyOrWhitespace) {
+        let txt = fLine.text
+        let mapping = getConfig().syntax_mapping
+        let find = mapping.find((obj: any) => txt.match(escapeStringRegexp(obj.first_line_pattern)))
+
+        if (find) {
+            let syntax = find.languageId
+
+            if (!languages.match(syntax, doc)) {
+                return languages.setTextDocumentLanguage(doc, syntax)
+            }
+        }
+    }
+}
+
+export function deactivate() {
+    //
+}
